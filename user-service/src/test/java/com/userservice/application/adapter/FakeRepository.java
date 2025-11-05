50831011530d24f51978124f77850c76fb3b8803
choizz156 <tkfka156@gmail.com>
2025-11-05 18:07:37 +0900

    test: 테스트 분리 및 중복 예외 테스트 추가 #3

---
+++ user-service/src/test/java/com/userservice/application/adapter/FakeRepository.java
@@ -0,0 +1,47 @@
+package com.userservice.application.adapter;
+
+import com.userservice.application.port.out.UserPersistencePort;
+import com.userservice.domain.model.User;
+
+public class FakeRepository implements UserPersistencePort {
+
+	@Override
+	public User findById(String id) {
+		return null;
+	}
+
+	@Override
+	public User save(User user) {
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+
+	}
+
+	@Override
+	public User findByEmail(String email) {
+		return null;
+	}
+
+	@Override
+	public User findBynickname(String nickname) {
+		return null;
+	}
+
+	@Override
+	public void withdraw(String id) {
+
+	}
+
+	@Override
+	public boolean existsPhoneNumber(String phoneNumber) {
+		return true;
+	}
+
+	@Override
+	public boolean existsNickname(String nickname) {
+		return true;
+	}
+}
--- user-service/src/test/java/com/userservice/application/adapter/UserApplicationTest.java
+++ user-service/src/test/java/com/userservice/application/adapter/UserApplicationTest.java
@@ -4,56 +4,30 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import com.common.model.persistence.BaseEntity.DeleteStatus;
-import com.userservice.domain.model.User;
-import com.userservice.domain.vo.Address;
-import com.userservice.domain.vo.UserRole;
-import com.userservice.infrastructure.jpa.adapter.UserPersistenceAdapter;
-import com.userservice.infrastructure.jpa.infrastructure.UserJpaRepository;
+import com.common.exception.CustomException;
 
 @DataJpaTest
 class UserApplicationTest {
 
-	@Autowired
-	private UserJpaRepository userJpaRepository;
-
-	@DisplayName("UserEntity를 저장할 수 있다.")
+	@DisplayName("회원 가입 시, 닉네임이 중복되면 예외를 던진다.")
 	@Test
 	void test1() throws Exception {
 		//given
-		var mockUser = User.builder()
-			.email("testuser@example.com")
-			.nickname("tester")
-			.name("테스트 유저")
-			.password("encodedPassword123!")
-			.address(
-				Address.builder()
-					.state("경기도")
-					.zipCode("12345")
-					.street("테스트로 123")
-					.city("성남시")
-					.details("101동 202호")
-					.build()
-			)
-			.role(UserRole.USER)
-			.phoneNumber("010-1234-5678")
-			.oauthId("kakao_1234567890")
-			.build();
-
-		var userPersistenceAdapter = new UserPersistenceAdapter(userJpaRepository);
-
-		//when
-		User result = userPersistenceAdapter.save(mockUser);
-
-		//then
-		assertThat(result.getCreatedAt()).isNotNull();
-		assertThat(result.getUpdatedAt()).isNotNull();
-		assertThat(result.getId()).isInstanceOf(String.class);
-		assertThat(result.getDeleteStatus()).isEqualByComparingTo(DeleteStatus.N);
-		assertThat(result.getRole()).isEqualByComparingTo(UserRole.USER);
+		UserApplication userApplication = new UserApplication(new FakeRepository(), null, null);
+		//when//then
+		assertThatThrownBy(() -> userApplication.verifyNickname("test_nickname"))
+			.isInstanceOf(CustomException.class);
 	}
 
+	@DisplayName("회원 가입 시, 연락처가 중복되면 예외를 던진다.")
+	@Test
+	void test2() throws Exception {
+		//given
+		UserApplication userApplication = new UserApplication(new FakeRepository(), null, null);
+		//when//then
+		assertThatThrownBy(() -> userApplication.verifyPhoneNumber("010-1111-1111"))
+			.isInstanceOf(CustomException.class);
+	}
 }
