89063dff5c5f260998c5599c16131a0aefbcbd58
choizz156 <tkfka156@gmail.com>
2025-11-05 13:26:39 +0900

    clear git cache

--- common/src/main/java/com/common/model/persistence/BaseEntity.java
+++ common/src/main/java/com/common/model/persistence/BaseEntity.java
@@ -69,4 +69,5 @@ public abstract class BaseEntity {
 	public enum DeleteStatus {
 		N, D
 	}
+
 }
---
+++ user-service/src/main/java/com/userservice/infrastructure/model/entity/UserEntity.java
@@ -0,0 +1,68 @@
+package com.userservice.infrastructure.model.entity;
+
+import com.common.model.persistence.BaseEntity;
+import com.userservice.domain.vo.UserRole;
+import com.userservice.infrastructure.model.vo.EmbeddedAddress;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+	private String oauthId;
+	private String name;
+	private String nickname;
+	private String email;
+	private String password;
+
+	//TODO: 판매자 등록시 인증??
+	@Enumerated(EnumType.STRING)
+	private UserRole role = UserRole.USER;
+
+	private String profileUrl;
+	private String phoneNumber;
+
+	@Embedded
+	private EmbeddedAddress address;
+
+	@Builder
+	public UserEntity(
+		String name,
+		String email,
+		String password,
+		UserRole role,
+		String profileUrl,
+		String phoneNumber,
+		EmbeddedAddress address,
+		String oauthId,
+		String nickname
+	) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.profileUrl = profileUrl;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.nickname = nickname;
+		this.oauthId = oauthId;
+	}
+
+	@Override
+	protected String getEntitySuffix() {
+		return UserEntity.class.getAnnotation(Table.class).name();
+	}
+}
