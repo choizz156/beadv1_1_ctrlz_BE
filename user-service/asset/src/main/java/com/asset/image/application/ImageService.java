2878d3301b84fbeb2b8e6f04044023f5b86eb942
choizz156 <tkfka156@gmail.com>
2025-11-06 15:14:59 +0900

    feat: 이미지 압축 구성 #16

--- user-service/asset/src/main/java/com/asset/image/application/ImageService.java
+++ user-service/asset/src/main/java/com/asset/image/application/ImageService.java
@@ -1,6 +1,9 @@
 package com.asset.image.application;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -17,11 +20,13 @@ import com.common.exception.CustomException;
 import com.common.exception.vo.FileExceptionCode;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -37,6 +42,7 @@ public class ImageService implements AssetService<Image> {
 	private final S3Client s3Client;
 	private final S3Presigner s3Presigner;
 	private final ImageRepository imageRepository;
+	private final ImageCompressor compressor;
 
 	@Override
 	public Image upload(MultipartFile file) {
@@ -47,36 +53,37 @@ public class ImageService implements AssetService<Image> {
 		String storedFileName = generateFileName(originalFileName);
 		String s3key = generateS3Key(storedFileName);
 
+		File compressedFile = compressor.compressToWebp(originalFileName, file);
+
 		try {
-			uploadToS3(file, s3key);
-
-			String s3Url = getS3Url(s3key);
-
-			Image image = Image.builder()
-				.originalFileName(originalFileName)
-				.storedFileName(storedFileName)
-				.s3Url(s3Url)
-				.s3Key(s3key)
-				.fileSize(file.getSize())
-				.contentType(file.getContentType())
-				.build();
-
-			return imageRepository.save(image);
+			uploadToS3(compressedFile, s3key);
 		} catch (Exception e) {
+			log.error("이미지 저장 실패 error: {} ", e.getMessage());
 			throw new RuntimeException(e);
 		}
-		return null;
+
+		String s3Url = getS3Url(s3key);
+		Image image = Image.builder()
+			.originalFileName(originalFileName)
+			.storedFileName(storedFileName)
+			.s3Url(s3Url)
+			.s3Key(s3key)
+			.fileSize(file.getSize())
+			.contentType(file.getContentType())
+			.build();
+
+		return imageRepository.save(image);
 	}
 
-	private void uploadToS3(MultipartFile file, String s3key) throws IOException {
+	private void uploadToS3(File file, String s3key) throws IOException {
 		PutObjectRequest request = PutObjectRequest.builder()
 			.bucket(bucketName)
 			.key(s3key)
-			.contentType(file.getContentType())
-			.contentLength(file.getSize())
+			.contentType(Files.probeContentType(file.toPath()))
+			.contentLength(file.length())
 			.build();
 
-		s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+		s3Client.putObject(request, RequestBody.fromInputStream(new FileInputStream(file), file.length()));
 	}
 
 	private void validateFile(MultipartFile file) {
