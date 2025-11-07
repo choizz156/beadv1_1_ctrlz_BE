4297dff10b414f75d1ee3e526ec3375b3b8f7f3e
choizz156 <tkfka156@gmail.com>
2025-11-07 11:42:14 +0900

    feat: cart 생성 시 feign 적용 #3

--- user-service/src/main/java/com/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/userservice/UserServiceApplication.java
@@ -2,12 +2,10 @@ package com.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.userservice", "com.common"})
-@EntityScan({"com.common.asset.image", "com.userservice.infrastructure"})
-@EnableJpaRepositories({"com.common.asset.image", "com.userservice.infrastructure"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
