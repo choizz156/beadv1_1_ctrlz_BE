88ce1db5307dc736c4f8d6a275f523bc79ec5f34
choizz156 <tkfka156@gmail.com>
2025-11-09 02:46:36 +0900

    feat: auth 모듈 user-service 이동 및 설정 #30

--- user-service/auth/src/test/java/com/auth/AuthApplicationTests.java
+++ user-service/auth/src/test/java/com/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.authservice;
+package com.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
