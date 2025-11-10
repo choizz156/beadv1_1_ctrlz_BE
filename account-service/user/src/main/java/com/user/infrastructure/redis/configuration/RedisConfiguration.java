ae96192512b84c0ecdba84dab07594e750339a93
choizz156 <tkfka156@gmail.com>
2025-11-11 08:39:03 +0900

    refactor: EmbeddedRedisConfiguration 위치 재 설정 #30

--- account-service/user/src/main/java/com/user/infrastructure/redis/configuration/RedisConfiguration.java
+++ account-service/user/src/main/java/com/user/infrastructure/redis/configuration/RedisConfiguration.java
@@ -1,4 +1,4 @@
-package com.common.configuration;
+package com.user.infrastructure.redis.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
