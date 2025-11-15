dc1a35370fbd8d4ba2c03f9271adccbfc4c903db
choizz156 <tkfka156@gmail.com>
2025-11-15 13:38:23 +0900

    kafa 임의 테스트

---
+++ domain-service/src/main/java/com/domainservice/UserCreatedListener.java
@@ -0,0 +1,28 @@
+package com.domainservice;
+
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.domainservice.domain.cart.service.CartService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@KafkaListener(
+	topics = {"${custom.cart.topic.command}"}
+)
+@Component
+public class UserCreatedListener {
+
+	private final CartService cartService;
+
+	@KafkaHandler
+	public void handler(@Payload CartCreateCommand  cartCreateCommand) {
+		cartService.addCart(cartCreateCommand.userId());
+		log.info(cartCreateCommand.toString());
+	}
+}
