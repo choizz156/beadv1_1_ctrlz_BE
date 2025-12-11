b93a1a608990b3a131ac6f3829541bbbcfbd05e4
choizz156 <tkfka156@gmail.com>
2025-12-11 15:35:34 +0900

    feat: 정산 배치 차등 수수료구현 및 Feign/Batch 재시도 전략 적용 #184

--- batch-service/src/main/java/com/batch/BatchApplication.java
+++ batch-service/src/main/java/com/batch/BatchApplication.java
@@ -3,7 +3,7 @@ package com.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.batch", "com.settlement", "com.search"})
+@SpringBootApplication(scanBasePackages = { "com.batch", "com.settlement", "com.search" })
 public class BatchApplication {
 
 	public static void main(String[] args) {
