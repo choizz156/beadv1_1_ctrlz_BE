543aeefd980db1b363cafee2291999e3f6b13a42
choizz156 <tkfka156@gmail.com>
2025-12-12 08:30:04 +0900

    refactor: time 수정 #184

--- batch-service/settlement/src/main/java/com/settlement/repository/SettlementRepository.java
+++ batch-service/settlement/src/main/java/com/settlement/repository/SettlementRepository.java
@@ -1,13 +1,8 @@
 package com.settlement.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.settlement.domain.entity.Settlement;
-import com.settlement.domain.entity.SettlementStatus;
 
 public interface SettlementRepository extends JpaRepository<Settlement, String> {
-
-	List<Settlement> findTop100BySettlementStatusOrderByCreatedAtAsc(SettlementStatus status);
 }
